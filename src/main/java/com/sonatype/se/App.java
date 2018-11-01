8f1749a9fac7e469e00e44f102dee6ef2f0b301b
HokieGeek <andres.f.perez@gmail.com>
2018-11-01 14:24:14 -0400

    Tester

---
+++ src/main/java/com/sonatype/se/App.java
@@ -0,0 +1,13 @@
+package com.sonatype.se;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
